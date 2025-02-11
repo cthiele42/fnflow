9f67ec5b1e33a4fb2d602d049093fc1245426ff3
cthiele42 <cthiele@ct42.de>
2025-02-11 21:23:20 +0100

    added license header

--- src/main/java/org/ct42/fnflow/FnPropsInjectingPostProcessor.java
+++ src/main/java/org/ct42/fnflow/FnPropsInjectingPostProcessor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ct42.fnflow;
 
 import org.ct42.fnflow.functions.ConfigurableFunction;
@@ -14,6 +30,9 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 
+/**
+ * @author Claas Thiele
+ */
 @Component
 @RegisterReflectionForBinding(ConfigurableFunction.class)
 public class FnPropsInjectingPostProcessor implements BeanPostProcessor {
