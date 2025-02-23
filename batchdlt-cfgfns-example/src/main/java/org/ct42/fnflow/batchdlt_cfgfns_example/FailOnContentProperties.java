737853d7737eb553e72ba9d62fb5806b13883a1a
cthiele42 <cthiele@ct42.de>
2025-02-23 21:02:20 +0100

    initial impl of batchdltcfgfns example

---
+++ batchdlt-cfgfns-example/src/main/java/org/ct42/fnflow/batchdlt_cfgfns_example/FailOnContentProperties.java
@@ -0,0 +1,8 @@
+package org.ct42.fnflow.batchdlt_cfgfns_example;
+
+import lombok.Data;
+
+@Data
+public class FailOnContentProperties {
+    private String failOn;
+}
---
+++ batchdlt-cfgfns-example/src/main/java/org/ct42/fnflow/batchdlt_cfgfns_example/PrependProperties.java
@@ -0,0 +1,8 @@
+package org.ct42.fnflow.batchdlt_cfgfns_example;
+
+import lombok.Data;
+
+@Data
+public class PrependProperties {
+    private String prefix;
+}
