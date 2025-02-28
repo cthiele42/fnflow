e1f7fe55b91ff9f211e3e0fc6668a33da3bcf601
cthiele42 <cthiele@ct42.de>
2025-02-28 08:18:56 +0100

    initial setup for json-processors-kafka

---
+++ fnflow-json-processors-kafka/src/main/java/org/ct42/fnflow/fnflow_json_processors_kafka/FnflowJsonProcessorsKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.ct42.fnflow.fnflow_json_processors_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnflowJsonProcessorsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnflowJsonProcessorsKafkaApplication.class, args);
+	}
+
+}
---
+++ fnflow-json-processors-kafka/src/test/java/org/ct42/fnflow/fnflow_json_processors_kafka/FnflowJsonProcessorsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ct42.fnflow.fnflow_json_processors_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnflowJsonProcessorsKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
