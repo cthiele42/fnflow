a2133dcff9d866643790a9075787108ba3b198a8
cthiele42 <cthiele@ct42.de>
2025-03-13 10:00:52 +0100

    add wait and timeout to deployment; use non-interactive mode for the test run

--- inttest/run_tests.sh
+++ inttest/run_tests.sh
@@ -9,5 +9,4 @@ helmfile -e local apply
 # run the tests
 cd ../inttest
 docker build --tag 'cypresscucumber' cypress-cucumber
-xhost + local:
-docker run -it --rm --network host -e DISPLAY -v .:/app -w /app -u $(id -u) --entrypoint cypress cypresscucumber:latest open --project .
+docker run -it --rm --network host -v .:/app -w /app -u $(id -u) cypresscucumber:latest
