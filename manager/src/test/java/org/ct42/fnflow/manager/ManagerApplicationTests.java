7ec8e36383af196347b8dd64dd260cb06f0840e3
cthiele42 <cthiele@ct42.de>
2025-03-31 11:38:06 +0200

    refactor manager test; add logging to error output of function composition

--- manager/src/test/java/org/ct42/fnflow/manager/ManagerApplicationTests.java
+++ manager/src/test/java/org/ct42/fnflow/manager/ManagerApplicationTests.java
@@ -16,7 +16,6 @@
 
 package org.ct42.fnflow.manager;
 
-import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
@@ -72,6 +71,7 @@ class ManagerApplicationTests {
 
 	@Test
 	void testCreatePod() {
+		//GIVEN
 		PipelineConfigDTO dto = new PipelineConfigDTO();
 		dto.setVersion("0.0.7");
 		dto.setSourceTopic("sourceTopic");
@@ -104,35 +104,32 @@ class ManagerApplicationTests {
 
 		dto.setPipeline(new PipelineConfigDTO.FunctionCfg[]{valiCfg, matchCfg, mergeCreate});
 
+		//WHEN
 		pipelineService.createOrUpdatePipeline("pipeline-name", dto);
 
-		PodList podList = kubernetesClient.pods()
-				.inNamespace("default")
-				.withLabel("app.kubernetes.io/instance", "pipeline-name")
-				.list();
-		then(podList.getItems()).hasSize(1);
-		then(podList.getItems().getFirst().getSpec().getContainers().getFirst().getArgs()).contains("--spring.cloud.stream.bindings.fnFlowComposedFnBean-in-0.destination=sourceTopic");
+		//THEN
+		thenCountOfPodRunningAndWithInstanceLabel("pipeline-name", 1);
+		thenPodWithInstanceNameArgumentsContains("pipeline-name", "--spring.cloud.stream.bindings.fnFlowComposedFnBean-in-0.destination=sourceTopic");
+		thenPodWithInstanceNameArgumentsContains("pipeline-name",
+				"--cfgfns.MergeCreate.merge.mappings.0.from=/name",
+				"--cfgfns.MergeCreate.merge.mappings.0.to=/name",
+				"--cfgfns.MergeCreate.merge.mappings.1.from=/name",
+				"--cfgfns.MergeCreate.merge.mappings.1.to=/product/fullName");
+		thenPipelineDeploymentIsCompleted("pipeline-name");
 
-		await().atMost(Duration.ofSeconds(180)).untilAsserted(() -> assertThat(pipelineService.getPipelineStatus("pipeline-name").getStatus()).isEqualTo(DeploymentStatusDTO.Status.COMPLETED));
-
+		//WHEN
 		dto.setSourceTopic("sourceTopicChanged");
 		pipelineService.createOrUpdatePipeline("pipeline-name", dto);
-		await().atMost(Duration.ofSeconds(180)).untilAsserted(() -> assertThat(pipelineService.getPipelineStatus("pipeline-name").getStatus()).isEqualTo(DeploymentStatusDTO.Status.COMPLETED));
 
-		await().untilAsserted(() -> assertThat(kubernetesClient.pods()
-				.inNamespace("default")
-				.withLabel("app.kubernetes.io/instance", "pipeline-name")
-				.list().getItems().stream().filter(p -> p.getStatus().getPhase().equals("Running")).collect(Collectors.toList())).hasSize(1));
-
-		then(kubernetesClient.pods()
-				.inNamespace("default")
-				.withLabel("app.kubernetes.io/instance", "pipeline-name")
-				.list().getItems().getFirst().getSpec().getContainers().getFirst().getArgs()).contains("--spring.cloud.stream.bindings.fnFlowComposedFnBean-in-0.destination=sourceTopicChanged");
-
+		//THEN
+		thenPipelineDeploymentIsCompleted("pipeline-name");
+		thenCountOfPodRunningAndWithInstanceLabel("pipeline-name", 1);
+		thenPodWithInstanceNameArgumentsContains("pipeline-name", "--spring.cloud.stream.bindings.fnFlowComposedFnBean-in-0.destination=sourceTopicChanged");
 	}
 
 	@Test
 	void testDeletePod() {
+		//GIVEN
 		PipelineConfigDTO dto = new PipelineConfigDTO();
 		dto.setVersion("0.0.1");
 		dto.setSourceTopic("sourceTopic");
@@ -155,28 +152,35 @@ class ManagerApplicationTests {
 
 		dto.setPipeline(new PipelineConfigDTO.FunctionCfg[]{valiCfg, matchCfg});
 
-		pipelineService.createOrUpdatePipeline("pipeline-name", dto);
+		pipelineService.createOrUpdatePipeline("pipeline-tobedeleted", dto);
 
-		PodList podList = kubernetesClient.pods()
+		thenCountOfPodRunningAndWithInstanceLabel("pipeline-tobedeleted", 1);
+		thenPodWithInstanceNameArgumentsContains("pipeline-tobedeleted", "--spring.cloud.stream.bindings.fnFlowComposedFnBean-in-0.destination=sourceTopic");
+		thenPipelineDeploymentIsCompleted("pipeline-tobedeleted");
+
+
+		//WHEN
+		pipelineService.deletePipeline("pipeline-tobedeleted");
+
+		//THEN
+		thenCountOfPodRunningAndWithInstanceLabel("pipeline-tobedeleted", 0);
+	}
+
+	private void thenCountOfPodRunningAndWithInstanceLabel(String instanceName, int count) {
+		await().atMost(Duration.ofSeconds(180)).untilAsserted(() -> assertThat(kubernetesClient.pods()
 				.inNamespace("default")
-				.withLabel("app.kubernetes.io/instance", "pipeline-name")
-				.list();
-		then(podList.getItems()).hasSize(1);
-		then(podList.getItems().getFirst().getSpec().getContainers().getFirst().getArgs()).contains("--spring.cloud.stream.bindings.fnFlowComposedFnBean-in-0.destination=sourceTopic");
+				.withLabel("app.kubernetes.io/instance", instanceName)
+				.list().getItems().stream().filter(p -> p.getStatus().getPhase().equals("Running")).collect(Collectors.toList())).hasSize(count));
+	}
 
-        then(podList.getItems().getFirst().getSpec().getContainers().getFirst().getArgs()).contains(
-                "--cfgfns.MergeCreate.merge.mappings.0.from=/name",
-                "--cfgfns.MergeCreate.merge.mappings.0.to=/name",
-                "--cfgfns.MergeCreate.merge.mappings.1.from=/name",
-                "--cfgfns.MergeCreate.merge.mappings.1.to=/product/fullName");
+	private void thenPipelineDeploymentIsCompleted(String pipelineName) {
+		await().atMost(Duration.ofSeconds(180)).untilAsserted(() -> assertThat(pipelineService.getPipelineStatus(pipelineName).getStatus()).isEqualTo(DeploymentStatusDTO.Status.COMPLETED));
+	}
 
-        await().atMost(Duration.ofSeconds(180)).untilAsserted(() -> assertThat(pipelineService.getPipelineStatus("pipeline-name").getStatus()).isEqualTo(DeploymentStatusDTO.Status.COMPLETED));
-
-		pipelineService.deletePipeline("pipeline-name");
-
-		await().untilAsserted(() -> assertThat(kubernetesClient.pods()
+	private void thenPodWithInstanceNameArgumentsContains(String name, String... args) {
+		then(kubernetesClient.pods()
 				.inNamespace("default")
-				.withLabel("app.kubernetes.io/instance", "pipeline-name")
-				.list().getItems()).isEmpty());
+				.withLabel("app.kubernetes.io/instance", name)
+				.list().getItems().getFirst().getSpec().getContainers().getFirst().getArgs()).contains(args);
 	}
 }
